0def58edbc11df2e73ae69f05dd6089b7faa95af
fran <fcojmoronreyes@gmail.com>
2024-06-07 21:22:01 +0200

    Add star icon to the search button

--- stardust_speculum/script.js
+++ stardust_speculum/script.js
@@ -111,6 +111,7 @@ function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
+
 window.onscroll = function() {
     const scrollToTopBtn = document.getElementById('scrollToTopBtn');
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
