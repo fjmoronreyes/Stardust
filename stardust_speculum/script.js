f2bb3a7074f5f2ab4297d6e84716dce5b356ed8d
fran <fcojmoronreyes@gmail.com>
2024-06-07 18:50:51 +0200

    Change the buttons to better responses

--- stardust_speculum/script.js
+++ stardust_speculum/script.js
@@ -96,3 +96,19 @@ function toggleDropdown() {
     const dropdownContent = document.getElementById('dropdownContent');
     dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
 }
+
+window.onscroll = function() { scrollFunction(); };
+
+function scrollFunction() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
